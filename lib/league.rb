0840d8ffc572f75106eba5ba1640f4fec90a1354
Jamison Griffith <jamison.griffith@gmail.com>
2021-04-07 09:35:28 -0600

    Initialize League

---
+++ lib/league.rb
@@ -0,0 +1,7 @@
+class League
+  attr_reader :name, :teams
+
+  def initialize(league_name)
+  end
+
+end
---
+++ spec/league_spec.rb
@@ -0,0 +1,21 @@
+require './lib/player'
+require './lib/team'
+require './lib/league'
+
+RSpec.describe League do
+
+  describe '#initialize' do
+    premier = League.new("Premier League")
+    roy = Player.new({name: "Roy Kent", position: "Center Midfielder" , salary: 1_000_000})
+    sam = Player.new({name: "Sam Obisanya", position: "Right-back Defender", salary: 600_000})
+    richmond = Team.new("AFC Richmond", "Ted Lasso", [roy, sam])
+    jamie = Player.new({name: "Jamie Tartt", position: "Striker", salary: 1_500_000})
+    fernandinho = Player.new({name: "Fernandinho", position: "Midfielder", salary: 5_200_000})
+    manchester = Team.new("Manchester City FC", "Pep Guardiola", [jamie, fernandinho])
+
+    it 'exists' do
+      expect(premier).to be_instance_of(League)
+    end
+
+  end
+end
