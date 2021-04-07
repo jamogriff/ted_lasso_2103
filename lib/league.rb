402bd9d2982296b17ccb9ff04897af7e05ad0808
Jamison Griffith <jamison.griffith@gmail.com>
2021-04-07 10:27:50 -0600

    Implement hacky most_expensive_player method

--- lib/league.rb
+++ lib/league.rb
@@ -26,4 +26,23 @@ class League
     hash
   end
 
+  # Pretty hacky way to fulfill implementation
+  # Utilizes #captains instead of iterating over all
+  # players, so not perfect.
+  def most_expensive_player
+    highest_paid = captains[-1]
+    second_highest = captains[-2]
+    most_expensive = []
+    if highest_paid.salary == second_highest.salary
+      most_expensive << highest_paid.name
+      most_expensive << second_highest.name
+    else
+      most_expensive << highest_paid.name
+    end
+    #require 'pry'; binding.pry
+  end
+
+  def players_by_salary_range
+  end
+
 end
--- spec/league_spec.rb
+++ spec/league_spec.rb
@@ -4,7 +4,7 @@ require './lib/league'
 
 RSpec.describe League do
 
-  describe '#initialize' do
+  describe 'iteration 3' do
     premier = League.new("Premier League")
     roy = Player.new({name: "Roy Kent", position: "Center Midfielder" , salary: 1_000_000})
     sam = Player.new({name: "Sam Obisanya", position: "Right-back Defender", salary: 600_000})
@@ -36,6 +36,35 @@ RSpec.describe League do
     it 'can sort players by team' do
       expect(premier.players_by_team).to be_instance_of(Hash)
       expect(premier.players_by_team[richmond]).to eq(["Roy Kent", "Sam Obisanya"])
+    end
+
+  end
+
+  describe 'iteration 4' do
+    premier = League.new("Premier League")
+    roy = Player.new({name: "Roy Kent", position: "Center Midfielder" , salary: 1_000_000})
+    sam = Player.new({name: "Sam Obisanya", position: "Right-back Defender", salary: 600_000})
+    richmond = Team.new("AFC Richmond", "Ted Lasso", [roy, sam])
+    jamie = Player.new({name: "Jamie Tartt", position: "Striker", salary: 1_500_000})
+    fernandinho = Player.new({name: "Fernandinho", position: "Midfielder", salary: 5_200_000})
+    manchester = Team.new("Manchester City FC", "Pep Guardiola", [jamie, fernandinho])
+    premier.add_team(richmond)
+    premier.add_team(manchester)
+
+    it 'returns most expensive player' do
+      expect(premier.most_expensive_player).to eq(["Fernandinho"])
+    end
+
+    it 'sorts players by salary range' do
+      result =  {
+            "Over 0M" => ["Sam Obisanya"],
+            "Over 1M" => ["Roy Kent", "Jamie Tartt"],
+            "Over 2M" => [],
+            "Over 3M" => [],
+            "Over 4M" => [],
+            "Over 5M" => ["Fernandinho"],
+          }
+      expect(premier.players_by_salary_range).to eq(result)
     end
 
   end
